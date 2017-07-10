f3e6b1b1b0228e3c5c70740aca4bb3c077351b00
zenda <1606726646@qq.com>
2017-07-10 20:51:13 +0800

    singer

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,11 +1,36 @@
 <template>
-	<div>
-		singer
-	</div>
+    <div>
+        singer
+    </div>
 </template>
 <script>
-    export default {}
+import {getSingerList} from 'api/singer'
+import {ERR_OK} from 'api/config'
+    export default {
+        data(){
+            return{
+                singers:[]
+            }
+        },
+        created(){
+            this._getSingerList()
+        },
+        methods:{
+            _getSingerList(){
+                getSingerList().then((res)=>{
+                    if(res.code===ERR_OK){
+                        this.singers=res.data.list
+                        
+                    }
+                })
+            }
+        }
+    }
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
-    
+    .singer
+        position:fixed
+        top:88px
+        bottom:0
+        width:100%
 </style>
