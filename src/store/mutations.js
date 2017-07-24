80557f792edc921969891c9f01c755a1285abe7f
zenda <1606726646@qq.com>
2017-07-24 20:19:52 +0800

    vuex for player

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -3,7 +3,10 @@ import * as types from './mutation-types'
 const mutations ={
     [types.SET_SINGER](state,singer){
         state.singer=singer
-    }
+    },
+    [types.SET_PLAYING_STATE](state,flag){
+        state.playing = flag
+    },
 }
 
 export default mutations
