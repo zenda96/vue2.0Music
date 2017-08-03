45ad59958ffa865ecb2fb69ada042c6af9cb692a
zenda <1606726646@qq.com>
2017-08-03 10:16:16 +0800

    suggest list

--- src/api/search.js
+++ src/api/search.js
@@ -9,3 +9,26 @@ export function getHotKey(){
     })
     return jsonp(url,data,options)
 }
+
+
+export function search(query,page,zhida){
+    const url = 'https://c.y.qq.com/soso/fcgi-bin/search_for_qq_cp'
+    const data = Object.assign({},commonParams,{
+        w:query,
+        zhidaqu:1,
+        catZhida: zhida? 1:0,
+        t:0,
+        flag:1,
+        ie:'utf-8',
+        sem:1,
+        aggr:0,
+        remoteplace:'txt.mqq.all',
+        perpage:20,
+        n:20,
+        p:page,
+        platform:'h5',
+        uid:0,
+        needNewCode:1,
+    })
+    return jsonp(url,data,options)
+}
--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -2,10 +2,10 @@
     <div>
         <div class="search">
             <div class="search-box-wrapper">
-                <search-box ref="searchBox"></search-box>
+                <search-box ref="searchBox" @query="onQueryChange"></search-box>
             </div>
         </div>
-        <div class="shortcut-wrapper">
+        <div class="shortcut-wrapper" v-show="!query">
             <div class="shortcut">
                 <div class="hot-key">
                     <h1 class="title">热门搜索</h1>
@@ -17,17 +17,22 @@
                 </div>
             </div>
         </div>
+        <div class="search-suggest" v-show="query">
+            <suggest :query="query"></suggest>
+        </div>
     </div>
 </template>
 <script>
     import SearchBox from 'base/search-box/search-box'
     import {getHotKey} from 'api/search'
     import {ERR_OK} from 'api/config'
+    import Suggest from 'components/suggest/suggest'
 
     export default {
         data(){
             return{
-                hotKey:[]
+                hotKey:[],
+                query:'',
             }
         },
         created(){
@@ -36,6 +41,9 @@
         methods:{
             addQuery(query){
                 this.$refs.searchBox.setQuery(query)
+            },
+            onQueryChange(query){
+                this.query = query
             },
             _getHotKey(){
                 getHotKey().then((res)=>{
@@ -46,7 +54,8 @@
             }
         },
         components:{
-            SearchBox
+            SearchBox,
+            Suggest
         }
     }
 </script>
